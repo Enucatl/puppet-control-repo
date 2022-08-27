f0c2ad16c340194d9540310a67e4bb3df7b6e2b5
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2022-08-27 10:50:36 +0200

    add certificates to traefik

--- manifests/site.pp
+++ manifests/site.pp
@@ -35,7 +35,6 @@ node default {
 }
 
 node 'dns.home.arpa' {
-
   $dns_variables = {
     'cpe_id' => Deferred('vault_key', [
       "${vault_addr}/v1/secret/data/dns",
@@ -53,27 +52,5 @@ node 'dns.home.arpa' {
 }
 
 node 'nuc10i7fnh.home.arpa' {
-  include dns::client
-
-  include vault_secrets::vault_cert
-
-  vault_cert { 'traefik-dashboard':
-    ensure            => present,
-    vault_uri         => "${vault_addr}/v1/pki_int/issue/home-dot-arpa",
-    auth_path         => 'cert',
-    cert_data         => {
-      'common_name' => "traefik-dashboard.${trusted['certname']}", 
-      'ttl'         =>  '48h',  # 2 day
-    },
-    renewal_threshold => 2,
-    ca_chain_file     => '/home/user/chain.pem',
-    cert_file     => '/home/user/cert.pem',
-    key_file     => '/home/user/key.pem',
-  }
-
-}
-
-node 'vm-debian.home.arpa' {
-  include tor_relay
-
+  create_resources(vault_cert, $vault_certs)
 }
