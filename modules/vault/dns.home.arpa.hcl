7cc57662942b4c3e302463da2f8e601632fd61cd
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2022-10-01 17:26:05 +0200

    add ldap config and admin vault policy

--- modules/vault/dns.home.arpa.hcl
+++
@@ -1,3 +0,0 @@
-path "secret/data/dns" {
-    capabilities = ["read"]
-}
---
+++ modules/vault/ldap-config.sh
@@ -0,0 +1,14 @@
+BINDPASS=$(vault kv get -mount=secret puppet | grep ldap_ro::password | tr -s ' ' | cut -d ' ' -f 2)
+
+vault write auth/ldap/config \
+    url="ldaps://ipa.home.arpa" \
+    userattr="uid" \
+    userdn="cn=users,cn=accounts,dc=home,dc=arpa" \
+    groupdn="cn=users,cn=accounts,dc=home,dc=arpa" \
+    groupfilter="(&(objectClass=person)(uid={{.Username}}))" \
+    groupattr="memberOf" \
+    binddn="uid=ldap_ro,cn=sysaccounts,cn=etc,dc=home,dc=arpa" \
+    bindpass="${BINDPASS}" \
+    certificate=@/etc/ssl/certs/root_2022_ca.pem \
+    insecure_tls=false \
+    starttls=false
