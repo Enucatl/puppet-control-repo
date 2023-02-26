50c72b4d65a5422310ccac8fe1629a1a6057297e
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2023-02-26 18:50:50 +0100

    fix variable name

--- modules/dns/manifests/client.pp
+++ modules/dns/manifests/client.pp
@@ -1,10 +1,10 @@
 class dns::client (
   String $server_ipv6,
-  String $server=$facts['networking']['dhcp'],
+  String $server_ipv4=$facts['networking']['dhcp'],
 ) {
     dnsmasq::conf { 'local-dns':
         ensure  => present,
-        content => "server=$server_ipv6\nserver=$server\nno-resolv",
+        content => "server=$server_ipv6\nserver=$server_ipv4\nno-resolv",
     }
 
     systemd::dropin_file { 'sshd.conf':
