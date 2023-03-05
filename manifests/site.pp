67563f22fbcfa2d7033301e6f872683659981aa6
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2023-03-05 11:18:47 +0100

    try username without @

--- manifests/site.pp
+++ manifests/site.pp
@@ -88,7 +88,7 @@ node 'nuc10i7fnh.home.arpa' {
   }
 
   postfix::hash { '/etc/postfix/sasl_passwd':
-    content => "${lookup('postfix::relayhost')} ${lookup('smtp_sasl_username')}:${lookup('smtp_sasl_password')}",
+    content => "${lookup('postfix::relayhost')} ${lookup('smtp_sasl_username_noat')}:${lookup('smtp_sasl_password')}",
     require => Class['postfix'],
   }
 
@@ -97,8 +97,7 @@ node 'nuc10i7fnh.home.arpa' {
   }
 
   postfix::conffile { 'sender_canonical':
-    content => "/^(.*)@(.*).home.arpa/    ${lookup('smtp_sasl_username_noat')}+\${1}.\${2}@gmail.com",
-    # content => "/.*/    ${lookup('smtp_sasl_username')}",
+    content => "/^(.*)@(.*).home.arpa/    ${lookup('smtp_sasl_username_noat')}+\${1}.\${2}.home.arpa@gmail.com",
   }
 
   file { '/var/lib/docker':
