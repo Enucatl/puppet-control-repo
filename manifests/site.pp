ed80ea1c28eb91fd979a7a8d4f6990554270eda6
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2023-03-05 11:33:51 +0100

    postfix on all hosts

--- manifests/site.pp
+++ manifests/site.pp
@@ -87,19 +87,6 @@ node 'nuc10i7fnh.home.arpa' {
     }
   }
 
-  postfix::hash { '/etc/postfix/sasl_passwd':
-    content => "${lookup('postfix::relayhost')} ${lookup('smtp_sasl_username')}:${lookup('smtp_sasl_password')}",
-    require => Class['postfix'],
-  }
-
-  postfix::conffile { 'recipient_canonical':
-    content => "/.+/    ${lookup('recipient_canonical')}",
-  }
-
-  postfix::conffile { 'sender_canonical':
-    content => "/^(.*)@(.*).(home.arpa)/    ${lookup('smtp_sasl_username')}+\${1}.\${2}.\${3}@gmail.com",
-  }
-
   file { '/var/lib/docker':
     ensure => 'directory',
     mode   => '0711',
