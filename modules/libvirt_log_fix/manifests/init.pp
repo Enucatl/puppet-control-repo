f8b2e755cd7a76f47efe0eb3cd269361402b4ca5
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2023-03-16 22:27:02 +0100

    also restart rsyslog.service

--- modules/libvirt_log_fix/manifests/init.pp
+++ modules/libvirt_log_fix/manifests/init.pp
@@ -2,4 +2,6 @@ class libvirt_log_fix {
   file { '/etc/rsyslog.d/10-rpc-libvirtd.conf':
     ensure  => present,
     content => 'puppet:///modules/libvirt/10-rpc-libvirtd.conf',
+    notify  =>  Service['rsyslog'],
+  }
 }
