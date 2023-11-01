3956e2f8ca614c6d177e88eb124437962a8384b2
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2023-11-01 23:01:22 +0100

    improve docstrings

--- scripts/external_node_classifier.py
+++ scripts/external_node_classifier.py
@@ -11,36 +11,30 @@ import yaml
 @click.argument("output_file", type=click.File("w"), default="-")
 @click.option("--domain", default="home.arpa")
 def main(certname, output_file, domain):
-    """
-    External Node Classifier (ENC) for Puppet
-    
-    This Python script acts as an External Node Classifier (ENC) for Puppet, allowing you
-    to define Puppet node configurations based on the certname of a client node.
-
-    Parameters:
-    certname (str): The certificate name of the client node.
-    output_file (str, optional): The file to write the YAML node configuration to. Defaults to standard output.
-    domain (str, optional): The default domain to use for matching environments. Defaults to "home.arpa".
-
-    Example Usage:
-    $ python enc.py <certname> <output_file> --domain <domain>
-
-    The script determines the environment for the Puppet node based on the certname
-    and outputs a YAML configuration file with the specified environment and any
-    additional parameters you may want to define.
-
-    For Puppet to use this ENC, specify it in your Puppet master's configuration file.
-
-    Note: This script is a basic example. You can extend it to support more complex
-    node classifications based on your infrastructure requirements.
+    """External Node Classifier (ENC) for Puppet.
 
     Args:
-    - certname (str): The certname of the Puppet client node.
-    - output_file (str): The path to the output file where the YAML node configuration will be saved.
-    - domain (str, optional): The default domain for environment matching.
+        certname (str): The certificate name of the client node.
+        output_file (str, optional): The file to write the YAML node
+            configuration to. Defaults to standard output.
+        domain (str, optional): The default domain to use for matching
+            environments. Defaults to "home.arpa".
+
+    Example Usage:
+        $ python enc.py <certname> --domain <domain> --output-file
+            <output_file>
+
+    If the node has a name that ends with `dev.domain`,
+    then its environment is set to dev, otherwise to production.
+
+    For Puppet to use this ENC, specify it in your Puppet master's
+    configuration file.
+
+    Note: This script is a basic example. You can extend it to support more
+    complex node classifications based on your infrastructure requirements.
 
     Returns:
-    None
+        None
     """
 
     output = {
