2cdfbd5d8cbba25e280b890b2b5943330280cd91
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2025-07-21 21:19:13 +0200

    docker after nftables

--- modules/nftables_config/manifests/init.pp
+++ modules/nftables_config/manifests/init.pp
@@ -3,8 +3,14 @@
 # This class manages nftables configuration files for specific rules.
 # It ensures the Docker-related accept rules are persistent and that
 # the main nftables configuration includes all files from /etc/nftables/.
+# It also ensures nftables starts before Docker.
 #
 class nftables_config {
+
+  # Ensure nftables package is installed
+  package { 'docker':
+    ensure => present,
+  }
 
   # Ensure nftables package is installed
   package { 'nftables':
@@ -23,17 +29,15 @@ class nftables_config {
   # Manage the main nftables configuration file to include other rule files
   file { '/etc/nftables.conf':
     ensure  => present,
-    # Use an EPP template to add the 'include' directive
     content => epp('nftables_config/nftables.conf.epp'),
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     require => Package['nftables'],
-    notify  => Service['nftables'], # Restart nftables service on change
+    notify  => Service['nftables'], # This will trigger a restart if content changes
   }
 
   # Manage the nftables rule file for DOCKER-USER chain
-  # This file will be included by the main /etc/nftables.conf due to the EPP template
   file { '/etc/nftables/docker_user_rules.nft':
     ensure  => present,
     source  => 'puppet:///modules/nftables_config/docker_user_rules.nft',
@@ -41,16 +45,42 @@ class nftables_config {
     group   => 'root',
     mode    => '0644',
     require => File['/etc/nftables'], # Ensure directory exists first
-    notify  => Service['nftables'],    # Restart nftables service on change
+    notify  => Service['nftables'],    # This will trigger a restart if content changes
   }
 
-  # Ensure nftables service is running and enabled
+  # Exec to trigger systemctl daemon-reload
+  # This is crucial for systemd to pick up changes in drop-in files before service management.
+  exec { 'systemd-daemon-reload':
+    command     => '/bin/systemctl daemon-reload',
+    refreshonly => true, # Only run when notified by the drop-in file
+  }
+
+  # Create a systemd drop-in file for the docker.service.
+  # This makes Docker wait for nftables to be ready before starting.
+  systemd::dropin_file { 'docker_after_nftables.conf':
+    unit    => 'docker.service',
+    content => "[Unit]\nRequires=nftables.service\nAfter=nftables.service\n",
+    # Notify systemd to reload its configuration when this file changes.
+    notify  => Exec['systemd-daemon-reload'],
+    # This drop-in is only useful if the docker package is installed.
+    require => Package['docker'],
+  }
+
+  # Now, manage the services. Puppet will handle the dependencies correctly.
+  # nftables will be started first.
   service { 'nftables':
-    ensure    => running,
-    enable    => true,
-    subscribe => [
-      File['/etc/nftables/docker_user_rules.nft'], # Subscribe to custom rules file
-      File['/etc/nftables.conf'],                # Subscribe to main config file
-    ],
+    ensure => running,
+    enable => true,
+    require => Package['nftables'],
+  }
+
+  # Thanks to the drop-in file, systemd now knows to start docker after nftables.
+  service { 'docker':
+    ensure  => running,
+    enable  => true,
+    require => Package['docker'],
+    # Explicitly subscribe to the drop-in to ensure the service is restarted
+    # with the new configuration if the drop-in file changes.
+    subscribe => Systemd::Dropin_file['docker_after_nftables.conf'],
   }
 }
