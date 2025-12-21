86e519f372f3cbb98cd055b9de0b5083cf3deb7f
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2025-12-21 20:04:17 +0100

    add ppas and sources to apt configuration, install docker, neovim

--- modules/packages/manifests/init.pp
+++ modules/packages/manifests/init.pp
@@ -1,36 +1,34 @@
-# modules/packages/manifests/init.pp
-#
-# Class to manage the installation of general system packages 
-# defined in Hiera.
-
 class packages (
-  Array $list = []  
+  Array $list    = [],
+  Array $ppas    = [],
+  Hash  $sources = {},
 ) {
-  # 2. Define default attributes for the package resources.
-  # We ensure the package is installed. We don't set a provider here 
-  # as Puppet is usually smart enough to determine apt, yum, etc., 
-  # based on the OS.
-  if empty($list) {
-    return()
+  include apt
+
+  # 1. Create PPA resources
+  $ppas.each |String $ppa_name| {
+    apt::ppa { $ppa_name: }
   }
 
-  $package_defaults = {
-    ensure => installed,
-  }
-  
-  # 3. Use create_resources to generate 'package' resources 
-  # for every item found in the $package_list.
-  # create_resources expects a hash where keys are resource titles 
-  # (package names) and values are parameter hashes (defaults).
-  # Since our Hiera list is an array, we first convert it to a hash 
-  # where the key and value are the package name, then merge the defaults.
-  
-  # Map the array [pkg1, pkg2] into a hash {pkg1 => {}, pkg2 => {}}
-  $package_hash = $list.reduce({}) |$memo, $pkg| {
-    $memo + {$pkg => {}}
+  # 2. Create Source resources (for Docker, etc.)
+  $sources.each |String $source_name, Hash $config| {
+    apt::source { $source_name:
+      * => $config, # This passes all Hiera keys as parameters
+    }
   }
 
-  # Create the resources
-  create_resources('package', $package_hash, $package_defaults)
+  # 3. The "Magic" Ordering
+  Apt::Ppa <| |>       -> Class['apt::update']
+  Apt::Source <| |>    -> Class['apt::update'] # Added this
+  Class['apt::update'] -> Package <| provider == 'apt' |>
 
+  # 4. Create the package resources
+  $list.each |String $package_name| {
+    if !defined(Package[$package_name]) {
+      package { $package_name:
+        ensure   => installed,
+        provider => 'apt',
+      }
+    }
+  }
 }
