738805a35d914bd8539286fe612757b5588ac87c
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2026-05-08 13:51:01 +0000

    fix: block noisy DHCP fact
    Manage Facter's config through the existing puppet_configuration class so agents can suppress the legacy dhcp_servers fact without disabling the broader networking fact group. This targets the dhcpcd stderr noise caused by Facter probing Ubuntu's dhcpcd-base binary while leaving the useful structured networking facts available for catalogs.

--- modules/puppet_configuration/manifests/init.pp
+++ modules/puppet_configuration/manifests/init.pp
@@ -5,6 +5,7 @@
 #
 # Parameters:
 #   - $settings: A hash containing Puppet configuration settings.
+#   - $facter_blocklist: A list of Facter facts to block.
 #
 # Example Usage:
 # puppet_configuration::settings:
@@ -23,11 +24,35 @@
 #
 class puppet_configuration (
   Hash $settings,
+  Array[String] $facter_blocklist = [],
 ) {
   # Determine the configuration directory based on the operating system family.
   $conf_dir = $facts['os']['family'] ? {
     'windows' => "${facts['common_appdata']}/PuppetLabs/puppet/etc",
     default   => '/etc/puppetlabs/puppet',
+  }
+
+  $facter_conf_dir = $facts['os']['family'] ? {
+    'windows' => "${facts['common_appdata']}/PuppetLabs/facter/etc",
+    default   => '/etc/puppetlabs/facter',
+  }
+
+  $facter_conf_dir_attributes = $facts['os']['family'] ? {
+    'windows' => {},
+    default   => {
+      owner => 'root',
+      group => 'root',
+      mode  => '0755',
+    },
+  }
+
+  $facter_conf_file_attributes = $facts['os']['family'] ? {
+    'windows' => {},
+    default   => {
+      owner => 'root',
+      group => 'root',
+      mode  => '0644',
+    },
   }
 
   # Set default values, including the path to the puppet.conf file.
@@ -37,4 +62,20 @@ class puppet_configuration (
 
   # Create resources using the ini_setting type to manage puppet.conf settings.
   create_resources(ini_setting, $settings, $defaults)
+
+  if !empty($facter_blocklist) {
+    $quoted_facter_blocklist = $facter_blocklist.map |String $fact| { "\"${fact}\"" }
+
+    file { $facter_conf_dir:
+      ensure => directory,
+      *      => $facter_conf_dir_attributes,
+    }
+
+    file { "${facter_conf_dir}/facter.conf":
+      ensure  => file,
+      content => "facts : {\n  blocklist : [ ${quoted_facter_blocklist.join(', ')} ]\n}\n",
+      require => File[$facter_conf_dir],
+      *       => $facter_conf_file_attributes,
+    }
+  }
 }
